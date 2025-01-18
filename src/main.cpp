1592980c5fb013fb138aec16f2327acefefd151f
Qasim Mustafa Hussain <qasim.mustafa11@gmail.com>
2025-01-18 12:45:09 +0100

    Added sensor read, convert to perc and serial print

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,32 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+const int sensorPin  = 15;  //sensor input pint
+int sensorVal;
+const int sensorMax = 2610, sensorMin = 1000;  //sensor ranges
+
+double calculate_moisture_perc(int sensorVal);
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
+  pinMode(sensorPin, INPUT);
+  Serial.println("Initialized");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  sensorVal = analogRead(sensorPin);
+  double moisturePerc = calculate_moisture_perc(sensorVal);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  Serial.print("Soil sensor val: ");
+  Serial.println(sensorVal);
+  Serial.print("Moisture percentage: ");
+  Serial.print(moisturePerc);
+  Serial.println("%");
+
+  delay(500);
+  }
+
+/* convert sensor analog value to moisture percentage */
+double calculate_moisture_perc(int sensorVal){
+  return (((float)(sensorMax - sensorVal)/(sensorMax - sensorMin)) * 100);
 }
