8034f77787469267d6921d664a66ab93fc36dfc9
Qasim Mustafa Hussain <qasim.mustafa11@gmail.com>
2025-05-08 15:56:28 +0200

    Added esp32 deep sleep

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,12 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
 
+//Misc Macros
+#define SLEEP_TIME_S 60
+#define S_TO_US 1000000
+#define SLEEP_TIME_US (SLEEP_TIME_S * S_TO_US)
+
+
 //MQTT MACROS
 #define WIFI_SSID "Qasim's 2.4"
 #define WIFI_PASS "0627923882"
@@ -14,7 +20,7 @@ WiFiClient espClient;
 PubSubClient client(espClient);
 char* MQTTSnakePlantTopic = "/study/snakePlant";
 char MQTTMessage[50] = {};
-const int writeDelay = 60 * 1* 1000;    //mqtt write time period = 1 minute
+const int writeDelay = 0;    //mqtt write time period (Set to zero as using deep sleep timer)
 
 //Sensor variables
 const int sensorPin  = 33;  //sensor input pint
@@ -29,6 +35,7 @@ const int LEDPin = 2;
 double calculate_moisture_perc(int sensorVal);
 void wifi_init();
 void MQTT_reconnect();
+void print_wakeup_reason();
 
 void setup() {
   Serial.begin(9600);
@@ -38,6 +45,13 @@ void setup() {
 
   pinMode(sensorPin, INPUT);
   pinMode(LEDPin, OUTPUT);
+
+  print_wakeup_reason();
+
+  Serial.print("Deep sleep time: ");
+  Serial.println(SLEEP_TIME_US);
+
+  esp_sleep_enable_timer_wakeup(SLEEP_TIME_US);    //deep sleep wake up every second
 
   Serial.println("Initialized");
 }
@@ -49,35 +63,41 @@ void loop() {
     digitalWrite(LEDPin, LOW);
     MQTT_reconnect();
   }
-    currentTime = millis();
 
-    if((currentTime - previousTime) > writeDelay){
+  currentTime = millis();
 
-      sensorSum = 0;
+  if((currentTime - previousTime) > writeDelay){    //Wake up delay set to 0 as using deep sleep timer
 
-      for (int i = 0; i < sensorAvgs; i++){
-        sensorSum += analogRead(sensorPin);
-      }
+    sensorSum = 0;
 
-      sensorSum /= sensorAvgs;
-      double moisturePerc = calculate_moisture_perc(sensorSum);
-      sprintf(MQTTMessage, "%.0f", moisturePerc);
+    for (int i = 0; i < sensorAvgs; i++){
+      sensorSum += analogRead(sensorPin);
+    }
 
-      Serial.print("Soil sensor val: ");
-      Serial.println(sensorSum);
+    sensorSum /= sensorAvgs;
+    double moisturePerc = calculate_moisture_perc(sensorSum);
+    sprintf(MQTTMessage, "%.0f", moisturePerc);
 
-      Serial.print("Moisture percentage: ");
-      Serial.print(moisturePerc);
-      Serial.println("%");
+    Serial.print("Soil sensor val: ");
+    Serial.println(sensorSum);
 
-      Serial.print("MQTT message: ");
-      Serial.println(MQTTMessage);
+    Serial.print("Moisture percentage: ");
+    Serial.print(moisturePerc);
+    Serial.println("%");
 
-      Serial.println("Publishing to MQTT server...");
-      client.publish(MQTTSnakePlantTopic, MQTTMessage);
+    Serial.print("MQTT message: ");
+    Serial.println(MQTTMessage);
 
-      previousTime = currentTime;
+    Serial.println("Publishing to MQTT server...");
+    client.publish(MQTTSnakePlantTopic, MQTTMessage);
+    delay(100);
+
+    previousTime = currentTime;
   }
+
+  Serial.println("Going to sleep...");
+  Serial.flush(); 
+  esp_deep_sleep_start();  
 }
 
 /* convert sensor analog value to moisture percentage */
@@ -113,5 +133,21 @@ void MQTT_reconnect(){
     }
 
     delay(1000);
+  }
+}
+
+void print_wakeup_reason(){
+  esp_sleep_wakeup_cause_t wakeup_reason;
+
+  wakeup_reason = esp_sleep_get_wakeup_cause();
+
+  switch(wakeup_reason)
+  {
+    case ESP_SLEEP_WAKEUP_EXT0 : Serial.println("Wakeup caused by external signal using RTC_IO"); break;
+    case ESP_SLEEP_WAKEUP_EXT1 : Serial.println("Wakeup caused by external signal using RTC_CNTL"); break;
+    case ESP_SLEEP_WAKEUP_TIMER : Serial.println("Wakeup caused by timer"); break;
+    case ESP_SLEEP_WAKEUP_TOUCHPAD : Serial.println("Wakeup caused by touchpad"); break;
+    case ESP_SLEEP_WAKEUP_ULP : Serial.println("Wakeup caused by ULP program"); break;
+    default : Serial.printf("Wakeup was not caused by deep sleep: %d\n",wakeup_reason); break;
   }
 }
