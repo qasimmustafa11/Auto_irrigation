84f0fdc497fd9af345979c0982fb8eba8b2cdead
Qasim Mustafa Hussain <qasim.mustafa11@gmail.com>
2025-05-10 18:56:59 +0200

    Added pump control based on soil moisture sensor reading

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,42 @@
 #include <Arduino.h>
 
 #define PUMP_PIN 2
+#define SENSOR_PIN 33
 
 int pinState = false;
+int currentTime, previousTime = 0;
+const int sensorMin = 1000, sensorMax = 2610, sensorSum = 0;
+int sensorAvgs = 1000;
 
-
+double calculate_moisture_perc(int sensorVal);
 
 void setup() {
   Serial.begin(9600);
   pinMode(PUMP_PIN, OUTPUT);
+  pinMode(SENSOR_PIN, INPUT);
 }
 
 void loop() {
-  delay(500);
-  if(pinState){
-    digitalWrite(PUMP_PIN, 0);
-    pinState = false;
+
+  //Read and average sensor value
+  int sensorSum = 0;
+  for (int i = 0; i < sensorAvgs; i++){
+    sensorSum += analogRead(SENSOR_PIN);
   }
-  else{
+  int sensorVal = sensorSum/sensorAvgs;
+  sensorVal = calculate_moisture_perc(sensorVal);
+  Serial.print("Sensor value: ");
+  Serial.print(sensorVal);
+  Serial.println(" %");
+
+  //If sensor val < 50%, turn on pump for 3 seconds
+  if(sensorVal<50%){
     digitalWrite(PUMP_PIN, 1);
-    pinState = true;
+    delay(3000);
   }
+}
+
+/* convert sensor analog value to moisture percentage */
+double calculate_moisture_perc(int sensorVal){
+  return (((float)(sensorMax - sensorVal)/(sensorMax - sensorMin)) * 100);
 }
